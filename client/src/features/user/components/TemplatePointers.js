b46245c723a260cd9d824cdfbf2733092d8a3715
wamiti8711@gmail.com <wamiti8711@gmail.com>
2023-11-21 10:07:27 +0300

    Feat: Dashboard

---
+++ client/src/features/user/components/TemplatePointers.js
@@ -0,0 +1,10 @@
+function TemplatePointers(){
+    return(
+        <>
+         <h1 className="text-2xl mt-8 font-bold"></h1>
+          <p className="py-2  mb-4"> Welcome to the Global payouts for affiliates and influencers in any country</p>
+        </>
+    )
+}
+
+export default TemplatePointers
