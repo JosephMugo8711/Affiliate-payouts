be8c5233377697a42d55fbdd7f56723a28533f82
wamiti8711@gmail.com <wamiti8711@gmail.com>
2023-11-21 16:19:52 +0300

    Feat:user model

---
+++ backend/app/models/userModel.js
@@ -0,0 +1,126 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const shortid = require('shortid');
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your  email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  country: {
+    type: String,
+    default: 'Kenya',
+  },
+  photo: {
+    type: String,
+    default:
+      'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541',
+  },
+  role: {
+    type: String,
+    enum: ['user', 'affiliate'],
+    default: 'affiliate'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and Save!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same!',
+    },
+  },
+  referralCode: {
+    type: String,
+    default: shortid.generate,
+    unique: true,
+  },
+  referrer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+  firstTransaction: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: false,
+    select: false,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.new) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+
+userSchema.pre('findOne', function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimeStamp;
+  }
+
+ 
+  return false;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
