04eb5df638aa0bdf215e55adfca06e9b0b054353
wamiti8711@gmail.com <wamiti8711@gmail.com>
2023-12-11 07:04:49 +0300

    Feat: readme

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
-
 import i18n from "i18next";
 import {  initReactI18next } from "react-i18next";
 import tEn from "./assets/locales/en/translation.json";
@@ -42,7 +41,4 @@ root.render(
   </React.Fragment>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
