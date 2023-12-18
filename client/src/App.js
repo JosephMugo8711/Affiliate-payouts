25f76f1a6a3623aafec967cf17a2207c5e094d3d
wamiti8711@gmail.com <wamiti8711@gmail.com>
2023-12-18 16:42:04 +0300

    Feat: auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,48 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { lazy, useEffect } from 'react'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { themeChange } from 'theme-change'
+import checkAuth from './app/auth';
+import initializeApp from './app/init';
+
+
 
 const Layout = lazy(() => import('./containers/Layout'));
 const Login = lazy(() => import('./features/user/Login'))
 const ForgotPassword = lazy(() => import('./features/user/RegisterPassword'))
 const Register = lazy(() => import('./features/user/Register'))
 
+
+// Initialize diff libraries
+initializeApp()
+
+
+// Check for login and initialize axios
+const token = checkAuth()
+
+
 function App() {
+
+  useEffect(() => {
+    themeChange(false)
+  }, [])
+
   return (
     <>
-      <Routes>
-         <Route path="/" element={<Register />} />
-         <Route path="/login" element={<Login/>}/>
-         <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/forgotpassword" element={<ForgotPassword/>}/>
 
-         <Route path="/app/*" element={<Layout />} />
+          // Any path starting with /app/ will match this route
+ 
+          <Route path="/app/*" element={<Layout />} />
 
-      </Routes>
+          <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
+
+        </Routes>
+      </Router>
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,18 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux-toolkit/store";
 import "./index.css";
+import SuspenseContent from "./containers/SuspenseContent";
 
 import App from "./App";
+import { Suspense } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <Suspense fallback={<SuspenseContent />}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </Suspense>
 );
 
